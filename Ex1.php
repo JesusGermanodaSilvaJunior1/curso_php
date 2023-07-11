6bd199e4c272729179a63ce0657778b9c86f8857
jesusgermano@live.com <jesusgermano@live.comgit config --global user.email jesusgermano@live.com>
2023-07-11 20:24:40 -0300

    primeiro programa

---
+++ Ex1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso Php</title>
+</head>
+<body>
+    <?php
+    // Legal, estou codificando meu primeiro programa em php
+    echo "<h2 aling='center'>Parabéns para mim, sou foda! </h2>";
+    ?>
+</body>
+</html>
