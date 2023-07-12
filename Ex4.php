aaf0df9c35d075bc496dc4d060731cf3aa4e96ba
jesusgermano@live.com <jesusgermano@live.comgit config --global user.email jesusgermano@live.com>
2023-07-12 01:15:11 -0300

    variaveis no meio de frases

---
+++ Ex4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso Php</title>
+</head>
+<body>
+    <?php
+        $time = 'Atlético Mineiro';
+        $ano = 2013;
+        $frase1 = "O $time é o melhor clube de futebol de Minas Gerais!";
+        $frase2 = "O $time foi campeão em $ano";
+        echo "<h3>$frase1</h3>";
+        echo "<h3>$frase2</h3>";
+    ?>
+</body>
+</html>
