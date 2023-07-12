72a275625d9b275b7cf2099594c0c274af4cbd5e
jesusgermano@live.com <jesusgermano@live.comgit config --global user.email jesusgermano@live.com>
2023-07-12 18:38:38 -0300

    Coloquei uma quebra de linha

---
+++ Ex5.1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso Php</title>
+</head>
+<body>
+    <?php
+        echo '<p align=center>Isto é um teste';
+        echo '<br>';
+        echo 'Coloquei uma quebra de linha'
+    ?>
+</body>
+</html>
