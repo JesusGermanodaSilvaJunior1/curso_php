64c2ca2c47b6337a2add6d3f03d5bed50803dfda
jesusgermano@live.com <jesusgermano@live.comgit config --global user.email jesusgermano@live.com>
2023-07-14 18:29:21 -0300

    variáveis válidas e váriaveis inválidas

---
+++ Ex8.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso Php</title>
+</head>
+<body>
+    <?php
+    #variaveis válidas
+        $nota1;
+        $casal20;
+        $galo_2013_campeao_libertadores;
+
+    #váriaveis inválidas
+        $5;
+        $20loja;
+        $2013_galo_campeão_libertadores;    
+    ?>
+</body>
+</html>
